8fdf161b9cbab72007e74a3b0a6766e63086e5d6
bhadreshjani <nilkanthim@gmail.com>
2016-10-06 03:19:49 -0700

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,13 @@
+
+public class Loops {
+	public static void main(String args[])
+	{
+	if(System.out.printf("bhadresh jani\n")==null)  //using printf with if
+	{
+	}
+	while(System.out.printf("bhadresh jani")==null)  //using printf with while
+	{
+	}
+	}
+	
+}
